ff04b96267617517c9deff70e619c6759c0b9e00
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-06-29 18:23:54 +0200

    axi_burst_splitter: Refactor counters into separate module

--- src/axi_burst_splitter.sv
+++ src/axi_burst_splitter.sv
@@ -11,6 +11,109 @@
 
 `include "axi/assign.svh"
 
+module axi_burst_splitter_counters #(
+  parameter int unsigned MAX_TXNS = 0,
+  parameter type id_t = logic
+) (
+  input  logic          clk_i,
+  input  logic          rst_ni,
+
+  input  id_t           alloc_id_i,
+  input  axi_pkg::len_t alloc_len_i,
+  input  logic          alloc_req_i,
+  output logic          alloc_gnt_o,
+
+  input  id_t           read_id_i,
+  output axi_pkg::len_t read_len_o,
+  input  logic          read_dec_i,
+  input  logic          read_req_i,
+  output logic          read_gnt_o
+);
+
+  typedef logic [$clog2(MAX_TXNS)-1:0] cnt_idx_t;
+  typedef logic [$bits(axi_pkg::len_t):0] cnt_t;
+  logic [MAX_TXNS-1:0]  cnt_dec, cnt_free, cnt_set;
+  cnt_t                 cnt_inp;
+  cnt_t [MAX_TXNS-1:0]  cnt_oup;
+  cnt_idx_t             cnt_free_idx, cnt_r_idx;
+  for (genvar i = 0; i < MAX_TXNS; i++) begin : gen_cnt
+    counter #(
+      .WIDTH ($bits(cnt_t))
+    ) i_cnt (
+      .clk_i,
+      .rst_ni,
+      .clear_i    (1'b0),
+      .en_i       (cnt_dec[i]),
+      .load_i     (cnt_set[i]),
+      .down_i     (1'b1),
+      .d_i        (cnt_inp),
+      .q_o        (cnt_oup[i]),
+      .overflow_o ()
+    );
+    assign cnt_free[i] = (cnt_oup[i] == '0);
+  end
+  assign cnt_inp = {1'b0, alloc_len_i} + 1;
+
+  lzc #(
+    .WIDTH  (MAX_TXNS),
+    .MODE   (1'b0)  // start counting at index 0
+  ) i_lzc (
+    .in_i     (cnt_free),
+    .cnt_o    (cnt_free_idx),
+    .empty_o  ()
+  );
+
+  logic idq_inp_req, idq_inp_gnt,
+        idq_oup_gnt, idq_oup_valid, idq_oup_pop;
+  id_queue #(
+    .ID_WIDTH ($bits(id_t)),
+    .CAPACITY (MAX_TXNS),
+    .data_t   (cnt_idx_t)
+  ) i_idq (
+    .clk_i,
+    .rst_ni,
+    .inp_id_i         (alloc_id_i),
+    .inp_data_i       (cnt_free_idx),
+    .inp_req_i        (idq_inp_req),
+    .inp_gnt_o        (idq_inp_gnt),
+    .exists_data_i    ('0),
+    .exists_mask_i    ('0),
+    .exists_req_i     (1'b0),
+    .exists_o         (/* keep open */),
+    .exists_gnt_o     (/* keep open */),
+    .oup_id_i         (read_id_i),
+    .oup_pop_i        (idq_oup_pop),
+    .oup_req_i        (read_req_i),
+    .oup_data_o       (cnt_r_idx),
+    .oup_data_valid_o (idq_oup_valid),
+    .oup_gnt_o        (idq_oup_gnt)
+  );
+  assign idq_inp_req = alloc_req_i & alloc_gnt_o;
+  assign alloc_gnt_o = idq_inp_gnt & |(cnt_free);
+  assign read_gnt_o = idq_oup_gnt & idq_oup_valid;
+  logic [8:0] read_len;
+  assign read_len = cnt_oup[cnt_r_idx] - 1;
+  assign read_len_o = read_len[7:0];
+
+  assign idq_oup_pop = read_req_i & read_gnt_o & read_dec_i & (read_len_o == 8'd0);
+  always_comb begin
+    cnt_dec = '0;
+    cnt_dec[cnt_r_idx] = read_req_i & read_gnt_o & read_dec_i;
+  end
+  always_comb begin
+    cnt_set = '0;
+    cnt_set[cnt_free_idx] = alloc_req_i & alloc_gnt_o;
+  end
+
+  `ifndef VERILATOR
+  // pragma translate_off
+  assume property (@(posedge clk_i) idq_oup_gnt |-> idq_oup_valid)
+    else $warning("Invalid output at ID queue, read not granted!");
+  // pragma translate_on
+  `endif
+
+endmodule
+
 module axi_burst_splitter #(
   // Maximum number of AXI read bursts outstanding at the same time
   parameter int unsigned MAX_READ_TXNS = 0,
@@ -147,67 +250,28 @@ module axi_burst_splitter #(
   `AXI_ASSIGN_TO_AR(slv_ar, slv);
   `AXI_ASSIGN_FROM_AR(mst, mst_ar);
 
-  typedef logic [$clog2(MAX_READ_TXNS)-1:0] r_cnt_idx_t;
-  logic [MAX_READ_TXNS-1:0]       r_cnt_dec, r_cnt_free, r_cnt_set;
-  logic [8:0]                     r_cnt_inp;
-  logic [MAX_READ_TXNS-1:0][8:0]  r_cnt_oup;
-  r_cnt_idx_t                     r_cnt_free_idx, r_cnt_r_idx;
-  for (genvar i = 0; i < MAX_READ_TXNS; i++) begin : gen_r_cnt
-    counter #(
-      .WIDTH (9)
-    ) i_cnt (
-      .clk_i,
-      .rst_ni,
-      .clear_i    (1'b0),
-      .en_i       (r_cnt_dec[i]),
-      .load_i     (r_cnt_set[i]),
-      .down_i     (1'b1),
-      .d_i        (r_cnt_inp),
-      .q_o        (r_cnt_oup[i]),
-      .overflow_o ()
-    );
-    assign r_cnt_free[i] = (r_cnt_oup[i] == '0);
-  end
-  assign r_cnt_inp = slv.ar_len + 1;
-  lzc #(
-    .WIDTH (MAX_READ_TXNS),
-    .MODE  (1'b0) // start counting at index 0
-  ) i_r_cnt_lzc (
-    .in_i     (r_cnt_free),
-    .cnt_o    (r_cnt_free_idx),
-    .empty_o  ()
-  );
-
-  logic r_cnt_idq_inp_req, r_cnt_idq_inp_gnt,
-        r_cnt_idq_oup_req, r_cnt_idq_oup_gnt, r_cnt_idq_oup_valid, r_cnt_idq_pop;
-  id_queue #(
-    .ID_WIDTH ($bits(id_t)),
-    .CAPACITY (MAX_READ_TXNS),
-    .data_t   (r_cnt_idx_t)
-  ) i_r_cnt_idq (
+  logic           r_cnt_alloc_req, r_cnt_alloc_gnt, r_cnt_dec, r_cnt_req, r_cnt_gnt;
+  axi_pkg::len_t  r_cnt_len;
+  axi_burst_splitter_counters #(
+    .MAX_TXNS (MAX_READ_TXNS),
+    .id_t     (id_t)
+  ) i_cntrs_r (
     .clk_i,
     .rst_ni,
-    .inp_id_i         (slv.ar_id),
-    .inp_data_i       (r_cnt_free_idx),
-    .inp_req_i        (r_cnt_idq_inp_req),
-    .inp_gnt_o        (r_cnt_idq_inp_gnt),
-    .exists_data_i    ('0),
-    .exists_mask_i    ('0),
-    .exists_req_i     (1'b0),
-    .exists_o         (),
-    .exists_gnt_o     (),
-    .oup_id_i         (mst.r_id),
-    .oup_pop_i        (r_cnt_idq_pop),
-    .oup_req_i        (r_cnt_idq_oup_req),
-    .oup_data_o       (r_cnt_r_idx),
-    .oup_data_valid_o (r_cnt_idq_oup_valid),
-    .oup_gnt_o        (r_cnt_idq_oup_gnt)
+    .alloc_id_i   (slv.ar_id),
+    .alloc_len_i  (slv.ar_len),
+    .alloc_req_i  (r_cnt_alloc_req),
+    .alloc_gnt_o  (r_cnt_alloc_gnt),
+    .read_id_i    (mst.r_id),
+    .read_len_o   (r_cnt_len),
+    .read_dec_i   (r_cnt_dec),
+    .read_req_i   (r_cnt_req),
+    .read_gnt_o   (r_cnt_gnt)
   );
 
   enum logic {ArIdle, ArBusy} ar_state_d, ar_state_q;
   always_comb begin
-    r_cnt_idq_inp_req = 1'b0;
-    r_cnt_set = '0;
+    r_cnt_alloc_req = 1'b0;
     ar_d = ar_q;
     ar_state_d = ar_state_q;
     mst.ar_valid = 1'b0;
@@ -215,21 +279,19 @@ module axi_burst_splitter #(
     slv.ar_ready = 1'b0;
     case (ar_state_q)
       ArIdle: begin
-        if (slv.ar_valid && |(r_cnt_free) && r_cnt_idq_inp_gnt) begin
+        if (slv.ar_valid && r_cnt_alloc_gnt) begin
           if (slv.ar_len == '0) begin // No splitting required -> feed through.
             mst_ar = slv_ar;
             mst.ar_valid = 1'b1;
             // As soon as downstream is ready, allocate a counter and acknowledge upstream.
             if (mst.ar_ready) begin
-              r_cnt_set[r_cnt_free_idx] = 1'b1;
-              r_cnt_idq_inp_req = 1'b1;
+              r_cnt_alloc_req = 1'b1;
               slv.ar_ready = 1'b1;
             end
           end else begin // Splitting required.
             // Store AR, allocate a counter, and acknowledge upstream.
             ar_d = slv_ar;
-            r_cnt_set[r_cnt_free_idx] = 1'b1;
-            r_cnt_idq_inp_req = 1'b1;
+            r_cnt_alloc_req = 1'b1;
             slv.ar_ready = 1'b1;
             // Try to feed first burst through.
             mst_ar = ar_d;
@@ -280,9 +342,8 @@ module axi_burst_splitter #(
   assign slv.r_user = mst.r_user;
   always_comb begin
     mst.r_ready = 1'b0;
-    r_cnt_dec = '0;
-    r_cnt_idq_oup_req = 1'b0;
-    r_cnt_idq_pop = 1'b0;
+    r_cnt_dec = 1'b0;
+    r_cnt_req = 1'b0;
     r_last_d = r_last_q;
     r_state_d = r_state_q;
     slv.r_last = 1'b0;
@@ -290,20 +351,15 @@ module axi_burst_splitter #(
 
     case (r_state_q)
       RFeedthrough: begin
-        // If downstream has an R beat ..
+        // If downstream has an R beat and the R counters can give us the remaining length of
+        // that burst, ...
         if (mst.r_valid) begin
-          // .. and the ID queue can give us the index of the counter ..
-          r_cnt_idq_oup_req = 1'b1;
-          if (r_cnt_idq_oup_gnt) begin
-            r_last_d = (r_cnt_oup[r_cnt_r_idx] == 9'd1);
-            // .. if this is the last beat of the burst, ..
-            if (r_last_d) begin
-              slv.r_last = 1'b1;
-              // pop the counter index from the ID queue.
-              r_cnt_idq_pop = 1'b1;
-            end
+          r_cnt_req = 1'b1;
+          if (r_cnt_gnt) begin
+            r_last_d = (r_cnt_len == 8'd0);
+            slv.r_last = r_last_d;
             // Decrement the counter.
-            r_cnt_dec[r_cnt_r_idx] = 1'b1;
+            r_cnt_dec = 1'b1;
             // Try to forward the beat upstream.
             slv.r_valid = 1'b1;
             if (slv.r_ready) begin
@@ -366,9 +422,6 @@ module axi_burst_splitter #(
   assume property (@(posedge clk_i) slv.ar_valid && slv.ar_ready |->
       |(slv.ar_cache & axi_pkg::CACHE_MODIFIABLE) || slv.ar_len > 15)
     else $warning("Splitting a non-modifiable AR burst with 16 beats or less, which violates the AXI spec.");
-  // Internals
-  assume property (@(posedge clk_i) r_cnt_idq_oup_gnt |-> r_cnt_idq_oup_valid)
-    else $error("Invalid output at R ID queue!");
   // Outputs
   assert property (@(posedge clk_i) mst.aw_valid |-> mst.aw_len == '0)
     else $fatal(1, "AW burst longer than a single beat emitted!");
