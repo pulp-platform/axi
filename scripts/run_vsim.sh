f9276a56ada2ac4a6b7cfd7da49ca41bcc270711
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-12-18 19:44:31 +0100

    scripts/run_vsim: Remove TBs of removed modules

--- scripts/run_vsim.sh
+++ scripts/run_vsim.sh
@@ -28,22 +28,5 @@ for DW in 8 16 32 64 128 256 512 1024; do
 	call_vsim tb_axi_to_axi_lite -GDW=$DW -t 1ps -c
 done
 
-test_axi_lite_xbar() {
-	call_vsim tb_axi_lite_xbar -GNUM_MASTER=$1 -GNUM_SLAVE=$2 -t 1ps -c
-}
-
-# regression test cases
-test_axi_lite_xbar 1 1
-test_axi_lite_xbar 4 9
-
-for NM in 1 2 3 4 8; do
-	test_axi_lite_xbar $NM 4
-done
-
-for NS in 1 2 3 4 8; do
-	test_axi_lite_xbar 4 $NS
-done
-
 call_vsim tb_axi_delayer
-call_vsim tb_axi_id_remap
 call_vsim tb_axi_atop_filter -GN_TXNS=1000
