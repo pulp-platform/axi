bbc5e02467cc4cf576a5ca26d8774113d7af2169
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-12-18 19:23:58 +0100

    axi_lite_join: Implement with `AXI_LITE_ASSIGN`

--- src/axi_lite_join.sv
+++ src/axi_lite_join.sv
@@ -9,39 +9,26 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 //
+// Andreas Kurth  <akurth@iis.ee.ethz.ch>
 // Fabian Schuiki <fschuiki@iis.ee.ethz.ch>
 
+`include "axi/assign.svh"
 
 /// A connector that joins two AXI-Lite interfaces.
 module axi_lite_join (
-    AXI_LITE.Slave  in,
-    AXI_LITE.Master out
+  AXI_LITE.Slave  in,
+  AXI_LITE.Master out
 );
 
-    `ifndef SYNTHESIS
-    initial begin
-        assert(in.AXI_ADDR_WIDTH == out.AXI_ADDR_WIDTH);
-        assert(in.AXI_DATA_WIDTH == out.AXI_DATA_WIDTH);
-    end
-    `endif
+  `AXI_LITE_ASSIGN(out, in);
 
-    assign out.aw_addr   = in.aw_addr;
-    assign out.aw_valid  = in.aw_valid;
-    assign out.w_data    = in.w_data;
-    assign out.w_strb    = in.w_strb;
-    assign out.w_valid   = in.w_valid;
-    assign out.b_ready   = in.b_ready;
-    assign out.ar_addr   = in.ar_addr;
-    assign out.ar_valid  = in.ar_valid;
-    assign out.r_ready   = in.r_ready;
-
-    assign in.aw_ready = out.aw_ready;
-    assign in.w_ready  = out.w_ready;
-    assign in.b_resp   = out.b_resp;
-    assign in.b_valid  = out.b_valid;
-    assign in.ar_ready = out.ar_ready;
-    assign in.r_data   = out.r_data;
-    assign in.r_resp   = out.r_resp;
-    assign in.r_valid  = out.r_valid;
+// pragma translate_off
+`ifndef VERILATOR
+  initial begin
+    assert(in.AXI_ADDR_WIDTH == out.AXI_ADDR_WIDTH);
+    assert(in.AXI_DATA_WIDTH == out.AXI_DATA_WIDTH);
+  end
+`endif
+// pragma translate_on
 
 endmodule
