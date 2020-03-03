2d9fa1dad69251dc372ed2739b50da7702ac854b
Andreas Kurth <akurth@iis.ee.ethz.ch>
2020-03-03 09:46:06 +0100

    axi_atop_filter: Improve comment on feedthrough

--- src/axi_atop_filter.sv
+++ src/axi_atop_filter.sv
@@ -99,10 +99,12 @@ module axi_atop_filter #(
           mst_req_o.aw_valid  = slv_req_i.aw_valid;
           slv_resp_o.aw_ready = mst_resp_i.aw_ready;
         end
-        // Feed W channel through if at least one AW request is outstanding or a new non atop AW is
-        // initiated.
-        if (((w_cnt_q > 0) && !w_cnt_q[COUNTER_WIDTH]) ||
-            (slv_req_i.aw_valid && slv_req_i.aw.atop[5:4] == axi_pkg::ATOP_NONE)) begin
+        // Feed W channel through if ..
+        if (((w_cnt_q > 0) && !w_cnt_q[COUNTER_WIDTH]) || // at least one AW request is outstanding
+                                                          // and there is no counter underflow, OR
+            (slv_req_i.aw_valid && slv_req_i.aw.atop[5:4] == axi_pkg::ATOP_NONE))
+                                                          // a new non-ATOP AW is being applied
+        begin
           mst_req_o.w_valid  = slv_req_i.w_valid;
           slv_resp_o.w_ready = mst_resp_i.w_ready;
         end
