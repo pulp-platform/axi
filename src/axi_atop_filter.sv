205f11f91fc9fa62b5ec5a55455004af24ff0976
Andreas Kurth <akurth@iis.ee.ethz.ch>
2020-03-09 08:01:56 +0100

    axi_atop_filter: :art: Fix suffix of enumeration type

--- src/axi_atop_filter.sv
+++ src/axi_atop_filter.sv
@@ -55,11 +55,11 @@ module axi_atop_filter #(
 
   typedef enum logic [2:0] {
     W_FEEDTHROUGH, BLOCK_AW, ABSORB_W, HOLD_B, INJECT_B, WAIT_R
-  } w_state_t;
-  w_state_t   w_state_d, w_state_q;
+  } w_state_e;
+  w_state_e   w_state_d, w_state_q;
 
-  typedef enum logic [1:0] { R_FEEDTHROUGH, INJECT_R, R_HOLD } r_state_t;
-  r_state_t   r_state_d, r_state_q;
+  typedef enum logic [1:0] { R_FEEDTHROUGH, INJECT_R, R_HOLD } r_state_e;
+  r_state_e   r_state_d, r_state_q;
 
   typedef logic [AxiIdWidth-1:0] id_t;
   id_t  id_d, id_q;
