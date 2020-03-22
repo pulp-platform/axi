773f9c00fbe6838eb8d1b0e7570701247e569b90
Florian Zaruba <zarubaf@iis.ee.ethz.ch>
2020-03-22 19:32:21 +0100

    axi_pkg: Add doc comments

--- src/axi_pkg.sv
+++ src/axi_pkg.sv
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2018 ETH Zurich, University of Bologna
+// Copyright (c) 2014-2020 ETH Zurich, University of Bologna
 //
 // Copyright and related rights are licensed under the Solderpad Hardware
 // License, Version 0.51 (the "License"); you may not use this file except in
@@ -12,31 +12,73 @@
 // Fabian Schuiki <fschuiki@iis.ee.ethz.ch>
 // Andreas Kurth  <akurth@iis.ee.ethz.ch>
 
+//! AXI Package
+/// Contains all necessary type definitions, constants and generally
+/// useful functions.
 package axi_pkg;
-
+  /// AXI Transaction Burst Type.
   typedef logic [1:0] burst_t;
+  /// AXI Transaction Response Type.
   typedef logic [1:0] resp_t;
+  /// AXI Transaction Cacheability Type.
   typedef logic [3:0] cache_t;
+  /// AXI Transaction Protection Type.
   typedef logic [2:0] prot_t;
+  /// AXI Transaction Quality of Service Type.
   typedef logic [3:0] qos_t;
+  /// AXI Transaction Region Type.
   typedef logic [3:0] region_t;
+  /// AXI Transaction Length Type.
   typedef logic [7:0] len_t;
+  /// AXI Transaction Size Type.
   typedef logic [2:0] size_t;
+  /// AXI5 Atomic Operation Type.
   typedef logic [5:0] atop_t; // atomic operations
-  typedef logic [3:0] nsaid_t; // non-secure address identifier
+  /// AXI5 Non-Secure Address Identifier.
+  typedef logic [3:0] nsaid_t;
 
+  /// In a fixed burst:
+  /// - The address is the same for every transfer in the burst.
+  /// - The byte lanes that are valid are constant for all beats in the burst. However, within those byte
+  /// lanes, the actual bytes that have `wstrb` asserted can differ for each beat in the burst.
+  /// This burst type is used for repeated accesses to the same location such as when loading or emptying
+  /// a FIFO.
   localparam BURST_FIXED = 2'b00;
+  /// In an incrementing burst, the address for each transfer in the burst is an increment of
+  /// the address for the previous transfer. The increment value depends on the size of the transfer. For
+  /// example, the address for each transfer in a burst with a size of 4 bytes is the previous address plus
+  /// four.
+  /// This burst type is used for accesses to normal sequential memory.
   localparam BURST_INCR  = 2'b01;
+  /// A wrapping burst is similar to an incrementing burst, except that the address wraps around to a lower
+  /// address if an upper address limit is reached.
+  /// The following restrictions apply to wrapping bursts:
+  /// - The start address must be aligned to the size of each transfer.
+  /// - The length of the burst must be 2, 4, 8, or 16 transfers.
   localparam BURST_WRAP  = 2'b10;
 
+  /// Normal access success. Indicates that a normal access has been successful. Can also indicate that an
+  /// exclusive access has failed.
   localparam RESP_OKAY   = 2'b00;
+  /// Exclusive access okay. Indicates that either the read or write portion of an exclusive access has been
+  /// successful.
   localparam RESP_EXOKAY = 2'b01;
+  /// Slave error. Used when the access has reached the slave successfully, but the slave wishes to return
+  /// an error condition to the originating master.
   localparam RESP_SLVERR = 2'b10;
+  /// Decode error. Generated, typically by an interconnect component, to indicate that there is no slave
+  /// at the transaction address.
   localparam RESP_DECERR = 2'b11;
 
+  /// When this bit is asserted, the interconnect, or any component, can delay the transaction reaching its
+  /// final destination for any number of cycles.
   localparam CACHE_BUFFERABLE = 4'b0001;
+  /// When HIGH, Modifiable indicates that the characteristics of
+  /// the transaction can be modified. When Modifiable is LOW, the transaction is Non-modifiable.
   localparam CACHE_MODIFIABLE = 4'b0010;
+  /// When this bit is asserted, read allocation of the transaction is recommended but is not mandatory.
   localparam CACHE_RD_ALLOC   = 4'b0100;
+  /// When this bit is asserted, write allocation of the transaction is recommended but is not mandatory.
   localparam CACHE_WR_ALLOC   = 4'b1000;
 
   // Maximum number of bytes per burst, as specified by `size` (see Table A3-2).
@@ -44,11 +86,12 @@ package axi_pkg;
     return 1 << size;
   endfunction
 
-  // An overly long address type lets us define functions that work generically for shorter
-  // addresses.  We rely on the synthesizer to optimize the unused bits away.
+  /// An overly long address type.
+  /// It lets us define functions that work generically for shorter
+  /// addresses.  We rely on the synthesizer to optimize the unused bits away.
   typedef logic [127:0] largest_addr_t;
 
-  // Aligned address of burst (see A3-51).
+  /// Aligned address of burst (see A3-51).
   function automatic largest_addr_t aligned_addr(largest_addr_t addr, size_t size);
     return (addr >> size) << size;
   endfunction
@@ -82,7 +125,7 @@ package axi_pkg;
     end
   endfunction
 
-  // MEMORY TYPE
+  /// Memory Type.
   typedef enum logic [3:0] {
     DEVICE_NONBUFFERABLE,
     DEVICE_BUFFERABLE,
@@ -98,6 +141,7 @@ package axi_pkg;
     WBACK_RWALLOCATE
   } mem_type_t;
 
+  /// Create an `AR_CACHE` field from a `mem_type_t` type.
   function automatic logic [3:0] get_arcache(mem_type_t mtype);
     unique case (mtype)
       DEVICE_NONBUFFERABLE              : return 4'b0000;
@@ -115,6 +159,7 @@ package axi_pkg;
     endcase // mtype
   endfunction
 
+  /// Create an `AW_CACHE` field from a `mem_type_t` type.
   function automatic logic [3:0] get_awcache(mem_type_t mtype);
     unique case (mtype)
       DEVICE_NONBUFFERABLE              : return 4'b0000;
@@ -133,36 +178,95 @@ package axi_pkg;
   endfunction
 
   // ATOP[5:0]
+  /// - Sends a single data value with an address.
+  /// - The target swaps the value at the addressed location with the data value that is
+  ///   supplied in the transaction.
+  /// - The original data value at the addressed location is returned.
+  /// - Outbound data size is 1, 2, 4, or 8 bytes.
+  /// - Inbound data size is the same as the outbound data size
   localparam ATOP_ATOMICSWAP  = 6'b110000;
+  /// - Sends two data values, the compare value and the swap value, to the addressed
+  ///   location. The compare and swap values are of equal size.
+  /// - The data value at the addressed location is checked against the compare value:
+  ///   - If the values match, the swap value is written to the addressed location.
+  ///   - If the values do not match, the swap value is not written to the addressed
+  ///     location.
+  /// - The original data value at the addressed location is returned.
+  /// - Outbound data size is 2, 4, 8, 16, or 32 bytes.
+  /// - Inbound data size is half of the outbound data size because the outbound data
+  ///   contains both compare and swap values, whereas the inbound data has only the
+  ///   original data value.
   localparam ATOP_ATOMICCMP   = 6'b110001;
   // ATOP[5:4]
+  /// Perform no atomic operation.
   localparam ATOP_NONE        = 2'b00;
+  /// - Sends a single data value with an address and the atomic operation to be performed.
+  /// - The target performs the operation using the sent data and value at the addressed
+  ///   location as operands.
+  /// - The result is stored in the address location.
+  /// - A single response is given without data.
+  /// - Outbound data size is 1, 2, 4, or 8 bytes.
   localparam ATOP_ATOMICSTORE = 2'b01;
+  /// Sends a single data value with an address and the atomic operation to be performed.
+  /// - The original data value at the addressed location is returned.
+  /// - The target performs the operation using the sent data and value at the addressed
+  ///   location as operands.
+  /// - The result is stored in the address location.
+  /// - Outbound data size is 1, 2, 4, or 8 bytes.
+  /// - Inbound data size is the same as the outbound data size.
   localparam ATOP_ATOMICLOAD  = 2'b10;
   // ATOP[3]
+  /// For AtomicStore and AtomicLoad transactions `AWATOP[3]` indicates the endianness that is required for the atomic
+  /// operation.
+  /// The value of `AWATOP[3]` applies to arithmetic operations only and is ignored for bitwise logical operations.
+  /// When deasserted, this bit indicates that the operation is little-endian.
   localparam ATOP_LITTLE_END  = 1'b0;
+  /// When asserted, this bit indicates that the operation is big-endian.
   localparam ATOP_BIG_END     = 1'b1;
   // ATOP[2:0]
+  /// The value in memory is added to the sent data and the result stored in memory.
   localparam ATOP_ADD   = 3'b000;
+  /// Every set bit in the sent data clears the corresponding bit of the data in memory.
   localparam ATOP_CLR   = 3'b001;
+  /// Bitwise exclusive OR of the sent data and value in memory.
   localparam ATOP_EOR   = 3'b010;
+  /// Every set bit in the sent data sets the corresponding bit of the data in memory.
   localparam ATOP_SET   = 3'b011;
+  /// The value stored in memory is the maximum of the existing value and sent data. This
+  /// operation assumes signed data.
   localparam ATOP_SMAX  = 3'b100;
+  /// The value stored in memory is the minimum of the existing value and sent data. This
+  /// operation assumes signed data.
   localparam ATOP_SMIN  = 3'b101;
+  /// The value stored in memory is the maximum of the existing value and sent data. This
+  /// operation assumes unsigned data.
   localparam ATOP_UMAX  = 3'b110;
+  /// The value stored in memory is the minimum of the existing value and sent data. This
+  /// operation assumes unsigned data.
   localparam ATOP_UMIN  = 3'b111;
 
   // `xbar_latency_e` and `xbar_cfg_t` are documented in `doc/axi_xbar.md`.
+  /// Slice on Demux AW channel.
   localparam logic [9:0] DemuxAw = (1 << 9);
+  /// Slice on Demux W channel.
   localparam logic [9:0] DemuxW  = (1 << 8);
+  /// Slice on Demux B channel.
   localparam logic [9:0] DemuxB  = (1 << 7);
+  /// Slice on Demux AR channel.
   localparam logic [9:0] DemuxAr = (1 << 6);
+  /// Slice on Demux R channel.
   localparam logic [9:0] DemuxR  = (1 << 5);
+  /// Slice on Mux AW channel.
   localparam logic [9:0] MuxAw   = (1 << 4);
+  /// Slice on Mux W channel.
   localparam logic [9:0] MuxW    = (1 << 3);
+  /// Slice on Mux B channel.
   localparam logic [9:0] MuxB    = (1 << 2);
+  /// Slice on Mux AR channel.
   localparam logic [9:0] MuxAr   = (1 << 1);
+  /// Slice on Mux R channel.
   localparam logic [9:0] MuxR    = (1 << 0);
+  /// Latency configuration for `axi_xbar`.
   typedef enum logic [9:0] {
     NO_LATENCY    = 10'b000_00_000_00,
     CUT_SLV_AX    = DemuxAw | DemuxAr,
@@ -172,6 +276,8 @@ package axi_pkg;
     CUT_MST_PORTS = MuxAw | MuxW | MuxB | MuxAr | MuxR,
     CUT_ALL_PORTS = 10'b111_11_111_11
   } xbar_latency_e;
+
+  /// Configuration for `axi_xbar`.
   typedef struct packed {
     int unsigned   NoSlvPorts;
     int unsigned   NoMstPorts;
@@ -186,12 +292,14 @@ package axi_pkg;
     int unsigned   NoAddrRules;
   } xbar_cfg_t;
 
-  // Commonly used rule types for `axi_xbar`: 64- and 32-bit addresses.
+  /// Commonly used rule types for `axi_xbar` (64-bit addresses).
   typedef struct packed {
     int unsigned idx;
     logic [63:0] start_addr;
     logic [63:0] end_addr;
   } xbar_rule_64_t;
+
+  /// Commonly used rule types for `axi_xbar` (32-bit addresses).
   typedef struct packed {
     int unsigned idx;
     logic [31:0] start_addr;
