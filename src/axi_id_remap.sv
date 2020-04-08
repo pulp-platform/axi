3d117a81e21c167492f595022041bdbd28f629a6
Wolfgang Rönninger <wroennin@student.ethz.ch>
2020-04-08 17:21:28 +0200

    fixup! axi_id_remap: Default IDs are zero

--- src/axi_id_remap.sv
+++ src/axi_id_remap.sv
@@ -115,7 +115,7 @@ module axi_id_remap #(
     .exists_oup_id_o ( wr_exists_id                            ),
     .exists_full_o   ( wr_exists_full                          ),
     .exists_o        ( wr_exists                               ),
-    .pop_oup_id_i    ( mst_resp_i.b.id[IdxWidth-1:0]           ), // TODO, check!!
+    .pop_oup_id_i    ( mst_resp_i.b.id[IdxWidth-1:0]           ),
     .pop_inp_id_o    ( slv_resp_o.b.id                         ),
     .pop_i           ( slv_resp_o.b_valid && slv_req_i.b_ready )
   );
@@ -135,7 +135,7 @@ module axi_id_remap #(
     .exists_oup_id_o  ( rd_exists_id                                                 ),
     .exists_full_o    ( rd_exists_full                                               ),
     .exists_o         ( rd_exists                                                    ),
-    .pop_oup_id_i     ( mst_resp_i.r.id[IdxWidth-1:0]                                ), // TODO, check!!
+    .pop_oup_id_i     ( mst_resp_i.r.id[IdxWidth-1:0]                                ),
     .pop_inp_id_o     ( slv_resp_o.r.id                                              ),
     .pop_i            ( slv_resp_o.r_valid && slv_req_i.r_ready && slv_resp_o.r.last )
   );
