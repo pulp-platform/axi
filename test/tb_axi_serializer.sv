de0613abe6de7397143372e49ccc6383273ef32a
Wolfgang Rönninger <wroennin@student.ethz.ch>
2020-04-22 13:08:20 +0200

    tb_axi_serializer: Add checker process

--- test/tb_axi_serializer.sv
+++ test/tb_axi_serializer.sv
@@ -33,8 +33,7 @@ module tb_axi_serializer #(
   localparam int unsigned AxiDataWidth =  64;    // Axi Data Width
   localparam int unsigned AxiUserWidth =  5;
   // Sim print config, how many transactions
-  localparam int unsigned PrintTnx = 500;
-
+  localparam int unsigned PrintTxn = 500;
 
   typedef axi_test::rand_axi_master #(
     // AXI interface parameters
@@ -146,6 +145,107 @@ module tb_axi_serializer #(
     rand_axi_slave.run();
   end
 
+  // Checker
+  typedef logic [AxiIdWidth-1:0] axi_id_t;
+  typedef logic [AxiIdWidth-1:0] axi_addr_t;
+  typedef logic [AxiIdWidth-1:0] axi_data_t;
+  typedef logic [AxiIdWidth-1:0] axi_strb_t;
+  typedef logic [AxiIdWidth-1:0] axi_user_t;
+  `AXI_TYPEDEF_AW_CHAN_T(aw_chan_t, axi_addr_t,  axi_id_t,  axi_user_t)
+  `AXI_TYPEDEF_W_CHAN_T(w_chan_t,  axi_data_t,  axi_strb_t,  axi_user_t)
+  `AXI_TYPEDEF_B_CHAN_T(b_chan_t,  axi_id_t,  axi_user_t)
+  `AXI_TYPEDEF_AR_CHAN_T(ar_chan_t,  axi_addr_t,  axi_id_t,  axi_user_t)
+  `AXI_TYPEDEF_R_CHAN_T(r_chan_t,  axi_data_t,  axi_id_t,  axi_user_t)
+  axi_id_t  aw_queue[$];
+  axi_id_t  ar_queue[$];
+  aw_chan_t aw_chan[$];
+  w_chan_t   w_chan[$];
+  b_chan_t   b_chan[$];
+  ar_chan_t ar_chan[$];
+  r_chan_t   r_chan[$];
+
+  initial begin : proc_checker
+    automatic axi_id_t  id_tmp;
+    automatic aw_chan_t aw_tmp;
+    automatic aw_chan_t aw_test;
+    automatic w_chan_t   w_tmp;
+    automatic w_chan_t   w_test;
+    automatic b_chan_t   b_tmp;
+    automatic b_chan_t   b_test;
+    automatic ar_chan_t ar_tmp;
+    automatic ar_chan_t ar_test;
+    automatic r_chan_t   r_tmp;
+    automatic r_chan_t   r_test;
+    forever begin
+      @(posedge clk);
+      #TestTime;
+      // All FIFOs get populated if there is something to put in
+      if (master.aw_valid && master.aw_ready) begin
+        `AXI_TO_AW(, aw_tmp, master)
+        aw_tmp.id = '0;
+        id_tmp    = master.aw_id;
+        aw_chan.push_back(aw_tmp);
+        aw_queue.push_back(id_tmp);
+        if (master.aw_atop[axi_pkg::ATOP_R_RESP]) begin
+          ar_queue.push_back(id_tmp);
+        end
+      end
+      if (master.w_valid && master.w_ready) begin
+        `AXI_TO_W(, w_tmp, master)
+        w_chan.push_back(w_tmp);
+      end
+      if (slave.b_valid && slave.b_ready) begin
+        id_tmp = aw_queue.pop_front();
+        `AXI_TO_B(, b_tmp, slave)
+        b_tmp.id = id_tmp;
+        b_chan.push_back(b_tmp);
+      end
+      if (master.ar_valid && master.ar_ready) begin
+        `AXI_TO_AR(, ar_tmp, master)
+        ar_tmp.id = '0;
+        id_tmp    = master.ar_id;
+        ar_chan.push_back(ar_tmp);
+        ar_queue.push_back(id_tmp);
+      end
+      if (slave.r_valid && slave.r_ready) begin
+        `AXI_TO_R(, r_tmp, slave)
+        if (slave.r_last) begin
+          id_tmp = ar_queue.pop_front();
+        end else begin
+          id_tmp = ar_queue[0];
+        end
+        r_tmp.id = id_tmp;
+        r_chan.push_back(r_tmp);
+      end
+      // Check that all channels match the expected response
+      if (slave.aw_valid && slave.aw_ready) begin
+        aw_test = aw_chan.pop_front();
+        `AXI_TO_AW(, aw_tmp, slave)
+        assert(aw_test == aw_tmp) else $error("AW Measured: %h Expected: %h", aw_tmp, aw_test);
+      end
+      if (slave.w_valid && slave.w_ready) begin
+        w_test = w_chan.pop_front();
+        `AXI_TO_W(, w_tmp, slave)
+        assert(w_test == w_tmp) else $error("AW Measured: %h Expected: %h", w_tmp, w_test);
+      end
+      if (master.b_valid && master.b_ready) begin
+        b_test = b_chan.pop_front();
+        `AXI_TO_B(, b_tmp, master)
+        assert(b_test == b_tmp) else $error("AW Measured: %h Expected: %h", b_tmp, b_test);
+      end
+      if (slave.ar_valid && slave.ar_ready) begin
+        ar_test = ar_chan.pop_front();
+        `AXI_TO_AR(, ar_tmp, slave)
+        assert(ar_test == ar_tmp) else $error("AW Measured: %h Expected: %h", ar_tmp, ar_test);
+      end
+      if (master.r_valid && master.r_ready) begin
+        r_test = r_chan.pop_front();
+        `AXI_TO_R(, r_tmp, master)
+        assert(r_test == r_tmp) else $error("AW Measured: %h Expected: %h", r_tmp, r_test);
+      end
+    end
+  end
+
   initial begin : proc_sim_progress
     automatic int unsigned aw         = 0;
     automatic int unsigned ar         = 0;
@@ -164,19 +264,19 @@ module tb_axi_serializer #(
         ar++;
       end
 
-      if ((aw % PrintTnx == 0) && ! aw_printed) begin
+      if ((aw % PrintTxn == 0) && ! aw_printed) begin
         $display("%t> Transmit AW %d of %d.", $time(), aw, NoWrites);
         aw_printed = 1'b1;
       end
-      if ((ar % PrintTnx == 0) && !ar_printed) begin
+      if ((ar % PrintTxn == 0) && !ar_printed) begin
         $display("%t> Transmit AR %d of %d.", $time(), ar, NoReads);
         ar_printed = 1'b1;
       end
 
-      if (aw % PrintTnx == 1) begin
+      if (aw % PrintTxn == 1) begin
         aw_printed = 1'b0;
       end
-      if (ar % PrintTnx == 1) begin
+      if (ar % PrintTxn == 1) begin
         ar_printed = 1'b0;
       end
 
