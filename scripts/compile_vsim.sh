f380cf153cf1a1d5633e5bc0460710f9f5f03d41
Andreas Kurth <akurth@iis.ee.ethz.ch>
2020-04-27 13:07:54 +0200

    scripts/compile_vsim: Suppress useless warning

--- scripts/compile_vsim.sh
+++ scripts/compile_vsim.sh
@@ -15,6 +15,10 @@
 
 set -e
 
-bender script vsim -t test --vlog-arg="-svinputport=compat" --vlog-arg="-override_timescale 1ns/1ps" > compile.tcl
+bender script vsim -t test \
+    --vlog-arg="-svinputport=compat" \
+    --vlog-arg="-override_timescale 1ns/1ps" \
+    --vlog-arg="-suppress 2583" \
+    > compile.tcl
 echo 'return 0' >> compile.tcl
 vsim -c -do 'exit -code [source compile.tcl]'
