4acac9501dbcd79c5ae7b3eb2772f0933d320a96
Andreas Kurth <akurth@iis.ee.ethz.ch>
2020-04-27 13:11:53 +0200

    scripts/compile_vsim: Respect `VSIM` environment variable

--- scripts/compile_vsim.sh
+++ scripts/compile_vsim.sh
@@ -15,10 +15,12 @@
 
 set -e
 
+[ ! -z "$VSIM" ] || VSIM=vsim
+
 bender script vsim -t test \
     --vlog-arg="-svinputport=compat" \
     --vlog-arg="-override_timescale 1ns/1ps" \
     --vlog-arg="-suppress 2583" \
     > compile.tcl
 echo 'return 0' >> compile.tcl
-vsim -c -do 'exit -code [source compile.tcl]'
+$VSIM -c -do 'exit -code [source compile.tcl]'
