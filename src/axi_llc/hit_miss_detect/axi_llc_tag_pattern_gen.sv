b38af5fab804b421826688d0e17dca20d190c1bd
Wolfgang Rönninger <wroennin@student.ethz.ch>
2020-07-02 17:36:10 +0200

    axi_llc:hit_miss_detect:tag_pattern_gen: Add inline documentation.

--- src/axi_llc/hit_miss_detect/axi_llc_tag_pattern_gen.sv
+++ src/axi_llc/hit_miss_detect/axi_llc_tag_pattern_gen.sv
@@ -10,46 +10,57 @@
 // specific language governing permissions and limitations under the License.
 //
 // File:   tag_pattern_gen.sv
-// Author: Wolfgang Roenninger <wroennin@student.ethz.ch>
+// Author: Wolfgang Roenninger <wroennin@iis.ee.ethz.ch>
 // Date:   11.06.2019
-//
-// Description: This is the Tag generation for the LLC
-//              Initialize the tag storage to a known value of all 0 using a march X
-//              BIST algorithm.
+
+/// This is the Tag generation for the LLC.
+/// Initialize the tag storage to a known value of all 0 using a march X
+/// BIST algorithm.
 //
 //              tag_t is shown, however fields do not mater as they are written with either '0 or '1
 //              typedef struct packed {
 //                logic                     val; // tag valid flag
 //                logic                     dit; // tag dirty flag
 //                logic [Cfg.TagLength-1:0] tag; // tag itself
-//              } tag_t;
-
-// register macros
-`include "common_cells/registers.svh"
-
+//              } pattern_t;
 module axi_llc_tag_pattern_gen #(
-  parameter axi_llc_pkg::llc_cfg_t Cfg       = -1,
-  parameter type                   pattern_t = logic,
-  parameter type                   way_ind_t = logic,
-  parameter type                   index_t   = logic
+  /// Stattic LLC configuration struct.
+  parameter axi_llc_pkg::llc_cfg_t Cfg = axi_llc_pkg::llc_cfg_t'{default: '0},
+  /// Pattern type for generation.
+  parameter type pattern_t = logic,
+  /// Way indicator type. Each way is checked indiviually. Bit position id the index of the way.
+  parameter type way_ind_t = logic,
+  /// Cache line index type (macro address).
+  parameter type index_t   = logic
 ) (
-  input  logic     clk_i,    // Clock
-  input  logic     rst_ni,   // Asynchronous reset active low
-
-  input  logic     valid_i,          // valid request to start BIST
-  output logic     ready_o,          // handshaking
-  // pattern output
-  output index_t   index_o,          // address for the sram
-  output pattern_t pattern_o,        // output pattern
-  output logic     req_o,            // request signal for the tag sram
-  output logic     we_o,             // write enable for tag sram
-  // BIST comparison input from the tag sram
-  input  way_ind_t bist_res_i,       // individual BIST comparison results
-  input  logic     bist_res_valid_i, // bist_res_i is valid
-  // BIST finished output
-  output way_ind_t bist_res_o,       // aggregated output of bist
-  output logic     eoc_o             // end of computation
+  /// Clock, positive edge triggered.
+  input  logic     clk_i,
+  /// Asynchronous reset, active low.
+  input  logic     rst_ni,
+  /// Valid request to start BIST.
+  input  logic     valid_i,
+  /// Module is ready to start BIST.
+  output logic     ready_o,
+  /// Pattern address for SRAM.
+  output index_t   index_o,
+  /// Output pattern.
+  output pattern_t pattern_o,
+  /// Request signal for the tag sram.
+  output logic     req_o,
+  /// Write enable for tag sram.
+  output logic     we_o,
+  /// Individual BIST comparison results.
+  input  way_ind_t bist_res_i,
+  /// `bist_res_i` is valid.
+  input  logic     bist_res_valid_i,
+  /// Aggregated output of bist.
+  output way_ind_t bist_res_o,
+  /// End of computation.
+  output logic     eoc_o
 );
+  `include "common_cells/registers.svh"
+  // Define the width of the pattern for assignment of bits.
+  localparam int unsigned PatternWidth = unsigned'($bits(pattern_t));
 
   // unit status
   logic         busy_d, busy_q, switch_busy;
@@ -73,26 +84,24 @@ module axi_llc_tag_pattern_gen #(
 
   always_comb begin
     // default assignments
-    busy_d        = busy_q;
-    switch_busy   = 1'b0;
-    bist_d        = bist_q;
-    update_bist   = 1'b0;
-    ready_o       = 1'b0;
-    req_o         = 1'b0;
-    we_o          = 1'b0;
-    eoc_o         = 1'b0;
+    busy_d      = busy_q;
+    switch_busy = 1'b0;
+    bist_d      = bist_q;
+    update_bist = 1'b0;
+    ready_o     = 1'b0;
+    req_o       = 1'b0;
+    we_o        = 1'b0;
+    eoc_o       = 1'b0;
     // counter signals
-    clear_cnt     = 1'b0;
-    en_cnt        = 1'b0;
-    load_cnt      = 1'b0; // will be used for BIST
-    data_cnt      = '0;
-    down_cnt      = 1'b0; // will be used for BIST
+    clear_cnt   = 1'b0;
+    en_cnt      = 1'b0;
+    load_cnt    = 1'b0; // will be used for BIST
+    data_cnt    = '0;
+    down_cnt    = 1'b0; // will be used for BIST
     // pattern to write (for now all zeros)
-    pattern_o.val = '0;
-    pattern_o.dit = '0;
-    pattern_o.tag = '0;
+    pattern_o   = pattern_t'{default: '0};
     // bist result
-    bist_res_d    = bist_res_q;
+    bist_res_d  = bist_res_q;
 
     // count and react according to BIST status
     if (busy_q) begin
@@ -100,24 +109,24 @@ module axi_llc_tag_pattern_gen #(
         // write all zeros counting up
         WZEROUP   : begin
           if (overflow_cnt) begin
-            clear_cnt     = 1'b1;
-            bist_d        = RZEROUP;
-            update_bist   = 1'b1;
+            clear_cnt   = 1'b1;
+            bist_d      = RZEROUP;
+            update_bist = 1'b1;
           end else begin
-            req_o         = 1'b1;
-            we_o          = 1'b1;
-            en_cnt        = 1'b1;
+            req_o  = 1'b1;
+            we_o   = 1'b1;
+            en_cnt = 1'b1;
           end
         end
         // read out all zeros counting up
         RZEROUP   : begin
           if (overflow_cnt) begin
-            clear_cnt     = 1'b1;
-            bist_d        = WONEUP;
-            update_bist   = 1'b1;
+            clear_cnt   = 1'b1;
+            bist_d      = WONEUP;
+            update_bist = 1'b1;
           end else begin
-            req_o         = 1'b1;
-            en_cnt        = 1'b1;
+            req_o  = 1'b1;
+            en_cnt = 1'b1;
           end
           // do checking of the bist result gets only saved, if one of the bist_res_i is zero
           bist_res_d = bist_res_q & bist_res_i;
@@ -125,55 +134,51 @@ module axi_llc_tag_pattern_gen #(
         // write all ones counting up
         WONEUP    : begin
           if (overflow_cnt) begin
-            load_cnt      = 1'b1;
-            data_cnt      = '1;
-            bist_d        = RONEDOWN;
-            update_bist   = 1'b1;
+            load_cnt    = 1'b1;
+            data_cnt    = '1;
+            bist_d      = RONEDOWN;
+            update_bist = 1'b1;
           end else begin
-            req_o         = 1'b1;
-            we_o          = 1'b1;
-            pattern_o.val = 1'b1;
-            pattern_o.dit = 1'b1;
-            pattern_o.tag = '1;
-            en_cnt        = 1'b1;
+            req_o     = 1'b1;
+            we_o      = 1'b1;
+            pattern_o = {PatternWidth{1'b1}};
+            en_cnt    = 1'b1;
           end
         end
         // read out all ones counting down
         RONEDOWN  : begin
           if (overflow_cnt) begin
-            load_cnt      = 1'b1;
-            data_cnt      = '1;
-            bist_d        = WZERODOWN;
-            update_bist   = 1'b1;
+            load_cnt    = 1'b1;
+            data_cnt    = '1;
+            bist_d      = WZERODOWN;
+            update_bist = 1'b1;
           end else begin
-            req_o         = 1'b1;
-            pattern_o.val = 1'b1;
-            pattern_o.dit = 1'b1;
-            pattern_o.tag = '1;
-            en_cnt        = 1'b1;
-            down_cnt      = 1'b1;
+            req_o     = 1'b1;
+            pattern_o = {PatternWidth{1'b1}};
+            en_cnt    = 1'b1;
+            down_cnt  = 1'b1;
           end
         end
         // write all zeros counting down
         WZERODOWN : begin
           if (overflow_cnt) begin
-            load_cnt      = 1'b1;
-            data_cnt      = '1;
-            bist_d        = RZERODOWN;
-            update_bist   = 1'b1;
+            load_cnt    = 1'b1;
+            data_cnt    = '1;
+            bist_d      = RZERODOWN;
+            update_bist = 1'b1;
           end else begin
-            req_o         = 1'b1;
-            we_o          = 1'b1;
-            en_cnt        = 1'b1;
-            down_cnt      = 1'b1;
+            req_o    = 1'b1;
+            we_o     = 1'b1;
+            en_cnt   = 1'b1;
+            down_cnt = 1'b1;
           end
         end
         // read all zeros counting down
         RZERODOWN : begin
           if (overflow_cnt) begin
-            clear_cnt     = 1'b1;
-            bist_d        = BISTEND;
-            update_bist   = 1'b1;
+            clear_cnt   = 1'b1;
+            bist_d      = BISTEND;
+            update_bist = 1'b1;
           end else begin
             req_o         = 1'b1;
             en_cnt        = 1'b1;
@@ -181,11 +186,11 @@ module axi_llc_tag_pattern_gen #(
           end
         end
         BISTEND   : begin
-          bist_d          = WZEROUP;
-          update_bist     = 1'b1;
-          eoc_o           = 1'b1;
-          busy_d          = 1'b0;
-          switch_busy     = 1'b1;
+          bist_d      = WZEROUP;
+          update_bist = 1'b1;
+          eoc_o       = 1'b1;
+          busy_d      = 1'b0;
+          switch_busy = 1'b1;
         end
         default : ; // not possible to come here
       endcase
