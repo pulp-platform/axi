e8c0a4c513c0ef3adb80d5b800c55d11886722a7
Andreas Kurth <akurth@iis.ee.ethz.ch>
2021-01-20 13:35:29 +0100

    typedef.svh: :art: Format

--- include/axi/typedef.svh
+++ include/axi/typedef.svh
@@ -103,18 +103,18 @@
   } resp_t;
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// AXI4+ATOP Define All Channels and Request/Response Structs
+// All AXI4+ATOP Channels and Request/Response Structs in One Macro
 //
-// This can be used whenever the user is not interested in "precise" control of
-// the naming of the individual channels.
+// This can be used whenever the user is not interested in "precise" control of the naming of the
+// individual channels.
 //
 // Usage Example:
 // `AXI_TYPEDEF_ALL(axi, addr_t, id_t, data_t, strb_t, user_t)
 //
-// This defines `axi_req_t` and `axi_resp_t` request/response structs as well as
-// `axi_aw_chan_t`, `axi_w_chan_t`, `axi_b_chan_t`, `axi_ar_chan_t`, and
-// `axi_r_chan_t` channel structs.
+// This defines `axi_req_t` and `axi_resp_t` request/response structs as well as `axi_aw_chan_t`,
+// `axi_w_chan_t`, `axi_b_chan_t`, `axi_ar_chan_t`, and `axi_r_chan_t` channel structs.
 `define AXI_TYPEDEF_ALL(__name, __addr_t, __id_t, __data_t, __strb_t, __user_t)                 \
   `AXI_TYPEDEF_AW_CHAN_T(__name``_aw_chan_t, __addr_t, __id_t, __user_t)                        \
   `AXI_TYPEDEF_W_CHAN_T(__name``_w_chan_t, __data_t, __strb_t, __user_t)                        \
@@ -123,6 +123,8 @@
   `AXI_TYPEDEF_R_CHAN_T(__name``_r_chan_t, __data_t, __id_t, __user_t)                          \
   `AXI_TYPEDEF_REQ_T(__name``_req_t, __name``_aw_chan_t, __name``_w_chan_t, __name``_ar_chan_t) \
   `AXI_TYPEDEF_RESP_T(__name``_resp_t, __name``_b_chan_t, __name``_r_chan_t)
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // AXI-Lite (4+Prot) Channel and Request/Response Structs
