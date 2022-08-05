250534b7e800a95a0be48433d3ce4954ca1aa03c
Andreas Kurth <akurth@iis.ee.ethz.ch>
2022-08-05 08:20:48 +0200

    axi_lite_mux: Reduce complexity of master port W channel
    See #243.

--- src/axi_lite_mux.sv
+++ src/axi_lite_mux.sv
@@ -283,7 +283,7 @@ module axi_lite_mux #(
     // W Channel
     //--------------------------------------
     // multiplexer
-    assign mst_w_chan      = (!w_fifo_empty && !b_fifo_full) ? slv_reqs_i[w_select].w        :   '0;
+    assign mst_w_chan      = slv_reqs_i[w_select].w;
     assign mst_w_valid     = (!w_fifo_empty && !b_fifo_full) ? slv_reqs_i[w_select].w_valid  : 1'b0;
     for (genvar i = 0; i < NoSlvPorts; i++) begin : gen_slv_w_ready
       assign slv_resps_o[i].w_ready =  mst_w_ready & ~w_fifo_empty &
