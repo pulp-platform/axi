7b0db8e4208427c6238d60d705aecd082fc8f902
Thomas Benz <tbenz@iis.ee.ethz.ch>
2023-05-08 10:25:24 +0200

    axi_pkg: Remove size function for better compatibility

--- src/axi_pkg.sv
+++ src/axi_pkg.sv
@@ -19,6 +19,28 @@
 //! AXI Package
 /// Contains all necessary type definitions, constants, and generally useful functions.
 package axi_pkg;
+    /// AXI Transaction Burst Width.
+  parameter int unsigned BurstWidth  = 32'd2;
+  /// AXI Transaction Response Width.
+  parameter int unsigned RespWidth   = 32'd2;
+  /// AXI Transaction Cacheability Width.
+  parameter int unsigned CacheWidth  = 32'd4;
+  /// AXI Transaction Protection Width.
+  parameter int unsigned ProtWidth   = 32'd3;
+  /// AXI Transaction Quality of Service Width.
+  parameter int unsigned QosWidth    = 32'd4;
+  /// AXI Transaction Region Width.
+  parameter int unsigned RegionWidth = 32'd4;
+  /// AXI Transaction Length Width.
+  parameter int unsigned LenWidth    = 32'd8;
+  /// AXI Transaction Size Width.
+  parameter int unsigned SizeWidth   = 32'd3;
+  /// AXI Lock Width.
+  parameter int unsigned LockWidth   = 32'd1;
+  /// AXI5 Atomic Operation Width.
+  parameter int unsigned AtopWidth   = 32'd6;
+  /// AXI5 Non-Secure Address Identifier.
+  parameter int unsigned NsaidWidth  = 32'd4;
 
   /// AXI Transaction Burst Width.
   typedef logic [1:0]  burst_t;
@@ -40,29 +62,6 @@ package axi_pkg;
   typedef logic [5:0]   atop_t; // atomic operations
   /// AXI5 Non-Secure Address Identifier.
   typedef logic [3:0]  nsaid_t;
-
-  /// AXI Transaction Burst Width.
-  parameter int unsigned BurstWidth  = $size(burst_t);
-  /// AXI Transaction Response Width.
-  parameter int unsigned RespWidth   = $size(resp_t);
-  /// AXI Transaction Cacheability Width.
-  parameter int unsigned CacheWidth  = $size(cache_t);
-  /// AXI Transaction Protection Width.
-  parameter int unsigned ProtWidth   = $size(prot_t);
-  /// AXI Transaction Quality of Service Width.
-  parameter int unsigned QosWidth    = $size(qos_t);
-  /// AXI Transaction Region Width.
-  parameter int unsigned RegionWidth = $size(region_t);
-  /// AXI Transaction Length Width.
-  parameter int unsigned LenWidth    = $size(len_t);
-  /// AXI Transaction Size Width.
-  parameter int unsigned SizeWidth   = $size(size_t);
-  /// AXI Lock Width.
-  parameter int unsigned LockWidth   = 32'd1;
-  /// AXI5 Atomic Operation Width.
-  parameter int unsigned AtopWidth   = $size(atop_t);
-  /// AXI5 Non-Secure Address Identifier.
-  parameter int unsigned NsaidWidth  = $size(nsaid_t);
 
   /// In a fixed burst:
   /// - The address is the same for every transfer in the burst.
